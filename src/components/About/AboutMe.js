b04428a4a3cb0d840a6d08ce5a8453dd69434dd0
yu <yuchiu2002@hotmail.com>
2017-11-16 20:27:18 -0500

    edited about me

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -13,7 +13,7 @@ class Contacts extends React.Component {
           </p>
           <p>
             I am actively seeking for a position that allows me to meaningfully impact
-            company growth, to expand upon my experience, and to apply my skills in an
+            company growth, to expand upon my experience, as well as applying my skills in an
             innovative, collaborative environment. <br/>Intrested in hiring me? Send me a message!
           </p>
         </div>
