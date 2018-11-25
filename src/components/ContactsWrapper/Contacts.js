e7888cf6b5ea459303a09e3130154003d2b2d0c2
yuchiu <yuchiu2002@hotmail.com>
2018-11-25 01:43:26 -0500

    finished email-form layout

--- src/components/ContactsWrapper/Contacts.js
+++ src/components/ContactsWrapper/Contacts.js
@@ -4,13 +4,31 @@ import "./Contacts.scss";
 
 export default () => (
   <div className="contacts-section">
-    <h4 className="">Get in touch with me</h4>
-    <h4 className="">To: steven2002yc@gmail.com</h4>
-    <br />
-    <h4 className="">Subject:</h4>
-    <input />
-    <textarea />
-    <h4 className="">From:</h4>
-    <input />
+    <div className="msg-header">
+      <h4 className="msg-header__title">Get in touch with me</h4>
+    </div>
+    <div className="msg-subject">
+      <div className="msg-subject__to">
+        <h4 className="msg-subject__to__h4">to:</h4> steven2002yc@gmail.com
+      </div>
+      <div className="msg-subject__section">
+        <h4 className="msg-subject__section__h4">Subject:</h4>
+        <input
+          className="msg-subject__section__input"
+          placeholder="Hello World!"
+        />
+      </div>
+    </div>
+    <div className="msg-body">
+      <h4 className="msg-body__h4">message:</h4>
+      <textarea className="msg-body__text" placeholder="Message Body" />
+    </div>
+    <div className="msg-sender">
+      <h4 className="msg-sender__h4">from:</h4>
+      <input className="msg-sender__mail-addr" placeholder="sender@email.com" />
+    </div>
+    <div className="msg-send-btn">
+      <button className="msg-send-btn__btn">Send</button>
+    </div>
   </div>
 );
